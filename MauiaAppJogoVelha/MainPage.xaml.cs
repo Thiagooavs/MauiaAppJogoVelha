50dca231e4233a9fe32bb8c0255d903ee6e60477
Usuario <Usuario@CPD03-M11>
2024-03-04 15:00:39 -0300

    Avanços no jogo da velha

--- MauiaAppJogoVelha/MainPage.xaml.cs
+++ MauiaAppJogoVelha/MainPage.xaml.cs
@@ -2,12 +2,34 @@
 {
     public partial class MainPage : ContentPage
     {
-
+        string vez = "O";
+        
         public MainPage()
         {
             InitializeComponent();
+
         }
 
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Button disparador = (Button)sender;//Fala que o sender é um botão
+
+            disparador.IsEnabled = false;
+
+            if(vez == "O")
+            {
+                disparador.Text = "O";
+                vez = "X";
+
+            }
+            else
+            {
+                disparador.Text = "X";
+                vez = "O";
+            }//fecha if, que controla a vez do jogador
+
+            if(btn10.Text == b)
+        }//Fecha Button_Clicked
     }
 
 }
