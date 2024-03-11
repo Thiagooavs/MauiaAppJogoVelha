624102dd59b05ef21c784c62c3c37b7444bf2d73
Usuario <Usuario@CPD03-M11>
2024-03-11 14:01:14 -0300

    Vitórias e empate

--- MauiaAppJogoVelha/MainPage.xaml.cs
+++ MauiaAppJogoVelha/MainPage.xaml.cs
@@ -3,18 +3,23 @@
     public partial class MainPage : ContentPage
     {
         string vez = "O";
+        int cont = 0;
+        int cc; 
         
         public MainPage()
         {
             InitializeComponent();
 
         }
-
+         
         private void Button_Clicked(object sender, EventArgs e)
         {
             Button disparador = (Button)sender;//Fala que o sender é um botão
-
+            cc = cont;
+            cont = cc + 1;
             disparador.IsEnabled = false;
+
+
 
             if(vez == "O")
             {
@@ -28,7 +33,115 @@
                 vez = "O";
             }//fecha if, que controla a vez do jogador
 
-            if(btn10.Text == b)
+            //X ganhou 
+            if(btn10.Text == "X" &&
+               btn11.Text == "X" &&
+               btn12.Text == "X")
+            {
+                DisplayAlert ("Parabens!!", "O X ganhou", "Fechar");
+            }
+            else if (btn20.Text == "X" &&
+                    btn21.Text == "X" &&
+                    btn22.Text == "X")
+            {
+                DisplayAlert("Parabens!!", "O X ganhou", "Fechar");
+            }
+            else if (btn30.Text == "X" &&
+                    btn31.Text == "X" &&
+                    btn32.Text == "X")
+            {
+                DisplayAlert("Parabens!!", "O X ganhou", "Fechar");
+            }
+            else if (btn10.Text == "X" &&
+                   btn20.Text == "X" &&
+                   btn30.Text == "X")
+            {
+                DisplayAlert("Parabens!!", "O X ganhou", "Fechar");
+            }
+            else if (btn11.Text == "X" &&
+                   btn21.Text == "X" &&
+                   btn31.Text == "X")
+            {
+                DisplayAlert("Parabens!!", "O X ganhou", "Fechar");
+            }
+            else if (btn12.Text == "X" &&
+                   btn22.Text == "X" &&
+                   btn32.Text == "X")
+            {
+                DisplayAlert("Parabens!!", "O X ganhou", "Fechar");
+            }
+            else if (btn10.Text == "X" &&
+                   btn21.Text == "X" &&
+                   btn32.Text == "X")
+            {
+                DisplayAlert("Parabens!!", "O X ganhou", "Fechar");
+            }
+            else if (btn12.Text == "X" &&
+                   btn21.Text == "X" &&
+                   btn30.Text == "X")
+            {
+                DisplayAlert("Parabens!!", "O X ganhou", "Fechar");
+            }
+            //O ganhou
+            if (btn10.Text == "O" &&
+               btn11.Text == "O" &&
+               btn12.Text == "O")
+            {
+                DisplayAlert("Parabens!!", "O O ganhou", "Fechar");
+            }
+            else if (btn20.Text == "O" &&
+                    btn21.Text == "O" &&
+                    btn22.Text == "O")
+            {
+                DisplayAlert("Parabens!!", "O O ganhou", "Fechar");
+            }
+            else if (btn30.Text == "O" &&
+                    btn31.Text == "O" &&
+                    btn32.Text == "O")
+            {
+                DisplayAlert("Parabens!!", "O O ganhou", "Fechar");
+            }
+            else if (btn10.Text == "O" &&
+                   btn20.Text == "O" &&
+                   btn30.Text == "O")
+            {
+                DisplayAlert("Parabens!!", "O O ganhou", "Fechar");
+            }
+            else if (btn11.Text == "O" &&
+                   btn21.Text == "O" &&
+                   btn31.Text == "O")
+            {
+                DisplayAlert("Parabens!!", "O O ganhou", "Fechar");
+            }
+            else if (btn12.Text == "O" &&
+                   btn22.Text == "O" &&
+                   btn32.Text == "O")
+            {
+                DisplayAlert("Parabens!!", "O O ganhou", "Fechar");
+            }
+            else if (btn10.Text == "O" &&
+                   btn21.Text == "O" &&
+                   btn32.Text == "O")
+            {
+                DisplayAlert("Parabens!!", "O O ganhou", "Fechar");
+            }
+            else if (btn12.Text == "O" &&
+                   btn21.Text == "O" &&
+                   btn30.Text == "O")
+            {
+                DisplayAlert("Parabens!!", "O O ganhou", "Fechar");
+            }
+
+            if (cont == 9)
+            {
+                DisplayAlert("Empate!!", "Ambos são péssimos", "Fechar");
+            }
+
+
+
+
+
+
         }//Fecha Button_Clicked
     }
 
